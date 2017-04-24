50ce1958f8d1617d6432578b0cb93057a8b07b21
Brian Cruz <cruz.s.brian@gmail.com>
2017-04-24 19:45:55 -0400

    Panel removal; fixed panel creation order; panel borders

--- panels.js
+++ panels.js
@@ -1,3 +1,5 @@
+var removing = false;
+
 function addPanel() {
 	// find largest leaf panel
 	var panels = $("#" + currentTab + " .panel.panel-leaf");
@@ -6,30 +8,74 @@ function addPanel() {
 	for (var i = 1; i < panels.length; i++) {
 		p = panels[i];
 		a = p.clientWidth * p.clientHeight;
-		if (a >= largestArea) {	// if they're the same I want the last one for aesthetic reasons
+		if (a  + 1000 >= largestArea) {	// if they're the same I want the last one for aesthetic reasons
 			largestPanel = p;
 			largestArea = a;
 		}
 	}
-	console.log(largestPanel);
+
+	// find largest panel id
+	largestId = 0;
+	for (var i = 0; i < panels.length; i++) {
+		id = parseInt($(panels[i]).attr("id"));
+		if (id > largestId) {
+			largestId = id;
+		}
+	}
 
 	// create a branch panel to replace it containing the old panel and the new one
-	var $parent = $(largestPanel.parentElement);
 	var $branchpanel = $(document.createElement("div")).addClass("panel");
 	var $oldpanel = $(largestPanel);
-	var $newpanel = $(document.createElement("div")).addClass("panel panel-leaf");
+	var $newpanel = $(document.createElement("div")).addClass("panel panel-leaf").attr("id", largestId + 1);
 
-	$newpanel.text("New Panel");
-
-	if (largestPanel.clientWidth >= largestPanel.clientHeight) {
+	if (largestPanel.clientWidth >= largestPanel.clientHeight * 1.25) { // *1.25 for better aspect ratio
 		$branchpanel.addClass("panel-branch-horizontal");
 	} else {
 		$branchpanel.addClass("panel-branch-vertical");
 	}
 
+	$branchpanel.insertAfter($oldpanel);
 	$oldpanel.detach().appendTo($branchpanel);
 	$newpanel.appendTo($branchpanel);
-
-	$branchpanel.appendTo($parent);
 }
 
+function removePanel() {
+	if (!removing) {
+		removing = true;
+		/*$(".panel.panel-leaf").mouseenter(function() {
+			// Create covering element directly over panel
+			$cover = $(document.createElement("div")).addClass("remove-cover").attr("id", this.id);
+			$cover.css({
+				top: this.offsetTop,
+				left: this.offsetLeft,
+				width: this.offsetWidth - 1,
+				height: this.offsetHeight - 1,
+			});
+			$cover.mouseleave(function () {
+				$cover.fadeOut(function() { $(".remove-cover#" + this.id).remove(); });
+			});
+
+			$cover.appendTo($(this.parentElement));
+			$cover.fadeIn();
+		});*/
+
+		$(".panel.panel-leaf").addClass("panel-removable");
+
+		$(".panel.panel-leaf").click(function() {
+			$branchpanel = $(this).parent();
+			$keeppanel = $(this).siblings();
+
+			$keeppanel.insertAfter($branchpanel);
+			$(this).remove();
+			$branchpanel.remove();
+		});
+
+		$("#removepanel").addClass("active");
+	} else {
+		removing = false;
+		$(".remove-cover").remove();
+		$(".panel.panel-leaf").removeClass("panel-removable");
+		$(".panel.panel-leaf").unbind("mouseenter");
+		$("#removepanel").removeClass("active");
+	}
+}
