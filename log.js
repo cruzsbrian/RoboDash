5ab7654b027de026bc12ffe3de8c1aabe8be15cc
Brian Cruz <cruz.s.brian@gmail.com>
2017-07-06 20:48:01 -0400

    Very basic logging features

---
+++ log.js
@@ -0,0 +1,11 @@
+function addLog(data) {
+	var $timestampSpan = $(document.createElement("span")).addClass("timestamp");
+	var $senderSpan = $(document.createElement("span")).addClass("sender");
+	var $messageSpan = $(document.createElement("span")).addClass("message");
+
+	$timestampSpan.text("[" + data.time + "]");
+	$senderSpan.text("[" + data.sender + "]");
+	$messageSpan.text(data.message);
+
+	$(".logview").append($timestampSpan, $senderSpan, $messageSpan, "<br>");
+}
