fcfe919646a88719d532f457a84e4d02ccfc503c
Brian Cruz <cruz.s.brian@gmail.com>
2017-09-11 13:53:29 -0400

    Switch to subject for logs instead of sender

--- log.js
+++ log.js
@@ -5,12 +5,12 @@ function makeLogView($panel) {
 
 function addLog(data) {
 	var $timestampSpan = $(document.createElement("span")).addClass("timestamp");
-	var $senderSpan = $(document.createElement("span")).addClass("sender");
+	var $subjectSpan = $(document.createElement("span")).addClass("subject");
 	var $messageSpan = $(document.createElement("span")).addClass("message");
 
-	$timestampSpan.text("[" + data.t + "]");
-	$senderSpan.text("[" + data.sender + "]");
+	$timestampSpan.text("[" + data.t + "s]");
+	$subjectSpan.text("[" + data.subject + "]");
 	$messageSpan.text(data.msg);
 
-	$(".logview").append($timestampSpan, $senderSpan, $messageSpan, "<br>");
+	$(".logview").append($timestampSpan, $subjectSpan, $messageSpan, "<br>");
 }
