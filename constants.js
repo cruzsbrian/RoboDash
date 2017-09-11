0e6161800d42b7c608646fc470b9ba6b038b5c5b
Brian Cruz <cruz.s.brian@gmail.com>
2017-09-11 13:55:13 -0400

    Make constants tab

---
+++ constants.js
@@ -0,0 +1,36 @@
+function addConstantToForm(constant) {
+	// allow leaving out constant to go to blank constant
+	constant = constant || {
+		key: '',
+		val: ''
+	};
+
+	// find highest constant number
+	var constantInputs = $(".constant-key");
+	var largestId = -1;
+	for (var i = 0; i < constantInputs.length; i++) {
+		var id = parseInt($(constantInputs[i]).attr("id"));
+		if (id > largestId) {
+			largestId = id;
+		}
+	}
+	console.log(largestId);
+
+	// make new inputs
+	var $keyInput = $("<input name='key" + (largestId + 1)
+		+ "' class='constant-key' id='" + (largestId + 1)
+		+ "' type='text' placeholder='key' value'" + constant.key
+		+ "'>");
+	var $valInput = $("<input name='val" + (largestId + 1)
+		+ "' class='constant-val' id='" + (largestId + 1)
+		+ "' type='text' placeholder='val' value'" + constant.val
+		+ "'>");
+
+	// add line break followed by the two tinputs before the plus button
+	// make sure not to add line breaks before the first set of inputs
+	if (largestId != -1) {
+		$("<br><br>").insertBefore($("a.add-constant"));
+	}
+	$keyInput.insertBefore($("a.add-constant"));
+	$valInput.insertBefore($("a.add-constant"));
+}
