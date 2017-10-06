9d9d281ec34b11490cc492d3ec91b736e5382e04
Brian Cruz <cruz.s.brian@gmail.com>
2017-10-06 18:09:47 -0400

    Columns for logs

--- log.js
+++ log.js
@@ -1,16 +1,28 @@
+var subjects = [];
+
 function makeLogView($panel) {
 	var $logView = $(document.createElement("div")).addClass("logview");
+	$logView.append($("<div class='column timestamp'>"));
+	$logView.append($("<div class='column subject'>"));
+	$logView.append($("<div class='column message'>"));
 	$panel.append($logView);
 }
 
 function addLog(data) {
-	var $timestampSpan = $(document.createElement("span")).addClass("timestamp");
-	var $subjectSpan = $(document.createElement("span")).addClass("subject");
-	var $messageSpan = $(document.createElement("span")).addClass("message");
+	var $timestampSpan = $("<span>");
+	var $subjectSpan = $("<span>");
+	var $messageSpan = $("<span>");
 
 	$timestampSpan.text("[" + data.t + "s]");
 	$subjectSpan.text("[" + data.subject + "]");
 	$messageSpan.text(data.msg);
 
-	$(".logview").append($timestampSpan, $subjectSpan, $messageSpan, "<br>");
+	$(".logview .timestamp").append($timestampSpan, $("<br>"));
+	$(".logview .subject").append($subjectSpan, $("<br>"));
+	$(".logview .message").append($messageSpan, $("<br>"));
+
+	// check if subject is in list and add it if it isn't
+	if (subjects.indexOf(data.subject) < 0) {
+		subjects.push(data.subject);
+	}
 }
