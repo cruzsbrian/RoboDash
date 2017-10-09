7da9095bcca5b13c196b712baa0477300569c2a3
Brian Cruz <cruz.s.brian@gmail.com>
2017-10-09 16:12:51 -0400

    Loading constants from robot

--- connection.js
+++ connection.js
@@ -1,53 +1,55 @@
 var ws;
 
 function addData(data) {
-	// determine if data is new graph data or a new log
-	if (data.type === "graph") {
-		// append the data object to graphData from graphs.js
-		graphData[graphData.length] = data.obj;
-		updateGraphs();
-	} else if (data.type === "log") {
-		// pass the object to addLog from log.js
-		addLog(data.obj);
-	}
+    // determine if data is new graph data or a new log
+    if (data.type === "graph") {
+        // append the data object to graphData from graphs.js
+        graphData[graphData.length] = data.obj;
+        updateGraphs();
+    } else if (data.type === "log") {
+        // pass the object to addLog from log.js
+        addLog(data.obj);
+    } else if (data.type === "constants") {
+        loadConstants(data.obj);
+    }
 }
 
 // show the connection dialog
 function showConnectionSettings() {
-	showSettings($(".connectionSettings"));
+    showSettings($(".connectionSettings"));
 
-	// have the form connect with submitted
-	$("#connectionSettingsForm").submit(function(e) {
-		connect();
-		e.preventDefault();
-	});
+    // have the form connect with submitted
+    $("#connectionSettingsForm").submit(function(e) {
+        connect();
+        e.preventDefault();
+    });
 }
 
 // get settings from dialog and open websocket
 function connect() {
-	var settingsForm = document.forms["connectionSettings"];
+    var settingsForm = document.forms["connectionSettings"];
 
-	var ip = settingsForm["ip"].value;
-	var port = settingsForm["port"].value;
+    var ip = settingsForm["ip"].value;
+    var port = settingsForm["port"].value;
 
-	ws = new WebSocket("ws://" + ip + ":" + port + "/");
+    ws = new WebSocket("ws://" + ip + ":" + port + "/");
 
-	ws.onopen = function(evt) {
-		console.log("connected");
-	};
+    ws.onopen = function(evt) {
+        console.log("connected");
+    };
 
-	ws.onmessage = function(evt) {
-		addData(JSON.parse(evt.data));
-	};
+    ws.onmessage = function(evt) {
+        addData(JSON.parse(evt.data));
+    };
 
-	hideSettings($(".connectionSettings"));
+    hideSettings($(".connectionSettings"));
 }
 
 function send(data) {
-	console.log(data);
-	if (ws && ws.readyState === ws.OPEN) {
-		ws.send(JSON.stringify(data));
-	} else {
-		console.log("Sending failed b/c WebSocket is not open.");
-	}
+    console.log(data);
+    if (ws && ws.readyState === ws.OPEN) {
+        ws.send(JSON.stringify(data));
+    } else {
+        console.log("Sending failed b/c WebSocket is not open.");
+    }
 }
--- constants.js
+++ constants.js
@@ -1,76 +1,90 @@
 function addConstantToForm(constant) {
-	// allow leaving out constant to go to blank constant
-	constant = constant || {
-		key: '',
-		val: ''
-	};
+    // allow leaving out constant to go to blank constant
+    constant = constant || {
+        key: '',
+        val: ''
+    };
 
-	// find highest constant number
-	var constantInputs = $(".constant-key");
-	var largestId = -1;
-	for (var i = 0; i < constantInputs.length; i++) {
-		var id = parseInt($(constantInputs[i]).attr("id"));
-		if (id > largestId) {
-			largestId = id;
-		}
-	}
+    // find highest constant number
+    var constantInputs = $(".constant-key");
+    var largestId = -1;
+    for (var i = 0; i < constantInputs.length; i++) {
+        var id = parseInt($(constantInputs[i]).attr("id"));
+        if (id > largestId) {
+            largestId = id;
+        }
+    }
 
-	// make new inputs
-	var $keyInput = $("<input name='key" + (largestId + 1)
-		+ "' class='constant-key' id='" + (largestId + 1)
-		+ "' type='text' placeholder='key' value'" + constant.key
-		+ "'>");
-	var $valInput = $("<input name='val" + (largestId + 1)
-		+ "' class='constant-val' id='" + (largestId + 1)
-		+ "' type='text' placeholder='val' value'" + constant.val
-		+ "'>");
+    // make new inputs
+    var $keyInput = $("<input name='key" + (largestId + 1)
+            + "' class='constant-key' id='" + (largestId + 1)
+            + "' type='text' placeholder='key' value='" + constant.key
+            + "'>");
+    var $valInput = $("<input name='val" + (largestId + 1)
+            + "' class='constant-val' id='" + (largestId + 1)
+            + "' type='text' placeholder='val' value='" + constant.val
+            + "'>");
 
-	// add line break followed by the two inputs before the plus button
-	// make sure not to add line breaks before the first set of inputs
-	if (largestId != -1) {
-		$("<br><br>").insertBefore($("a.add-constant"));
-	}
-	$keyInput.insertBefore($("a.add-constant"));
-	$valInput.insertBefore($("a.add-constant"));
+    // add line break followed by the two inputs before the plus button
+    // make sure not to add line breaks before the first set of inputs
+    if (largestId != -1) {
+        $("<br><br>").insertBefore($("a.add-constant"));
+    }
+    $keyInput.insertBefore($("a.add-constant"));
+    $valInput.insertBefore($("a.add-constant"));
 }
 
+function clearConstants() {
+    $("#constantsForm input").remove();
+    $("#constantsForm br").remove();
+}
+
+function loadConstants(cList) {
+    if (cList.length > 0) {
+        clearConstants();
+    }
+
+    for (var i = 0; i < cList.length; i++) {
+        addConstantToForm(cList[i]);
+    }
+}
 
 // returns an array of all the constants
 function collectConstants() {
-	var result = [];
+    var result = [];
 
-	var constantsForm = document.forms["constants"];
-	var constantInputs = $(".constant-key");
-	for (var i = 0; i < constantInputs.length; i++) {
-		var id = $(constantInputs[i]).attr("id");
+    var constantsForm = document.forms["constants"];
+    var constantInputs = $(".constant-key");
+    for (var i = 0; i < constantInputs.length; i++) {
+        var id = $(constantInputs[i]).attr("id");
 
-		var key = constantsForm["key" + id].value;
-		var val = constantsForm["val" + id].value;
-		result.push({key: key, val: val});
-	}
+        var key = constantsForm["key" + id].value;
+        var val = constantsForm["val" + id].value;
+        result.push({key: key, val: val});
+    }
 
-	return result;
+    return result;
 }
 
 // send constants to the robot
 // make defaults true if they should become defaults
 function sendConstants(defaults) {
-	defaults = defaults || false;
+    defaults = defaults || false;
 
-	var data = {
-		type: "constants",
-		defaults: defaults,
-		obj: collectConstants()
-	};
+    var data = {
+        type: "constants",
+        defaults: defaults,
+        obj: collectConstants()
+    };
 
-	send(data);
+    send(data);
 }
 
 function requestConstantDefaults() {
-	var data = {
-		type: "request",
-		obj: "constants"
-	};
+    var data = {
+        type: "request",
+        obj: "constants"
+    };
 
-	send(data);
+    send(data);
 }
