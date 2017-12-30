e4d488860da4c0a529b02dd497fc071415160137
Brian Cruz <cruz.s.brian@gmail.com>
2017-12-30 17:24:16 -0500

    Fix plotly modebars covering file menu

--- ui/tabs.js
+++ ui/tabs.js
@@ -1,22 +1,18 @@
 var currentTab = "";
 
 function openTab(tabName) {
-	currentTab = tabName;
+    currentTab = tabName;
 
-	$(".tabcontent").hide();
-	$(".toolbar-button").removeClass("active");
-	$("#" + currentTab).css("display", "flex");
-	$("#" + currentTab + "tab").addClass("active");
+    $(".tabcontent").hide();
+    $(".toolbar-button").removeClass("active");
+    $("#" + currentTab).css("display", "flex");
+    $("#" + currentTab + "tab").addClass("active");
 }
 
-function showDropdown() {
-	console.log("opening dropdown");
-	$(".dropdown").addClass("active");
-	
-	$(document).ready(function() {
-		$("body").click(function() {
-			$(".dropdown").removeClass("active");
-			$("body").unbind("click");
-		});
-	});
+function toggleDropdown() {
+    if ($(".dropdown").hasClass("active")) {
+        $(".dropdown").removeClass("active");
+    } else {
+        $(".dropdown").addClass("active");
+    }
 }
